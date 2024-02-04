2e9d643ad4756d88e52db7a163bc0f43720be6ab
FOFANA <fofana36u@etu.univ-lorraine.fr>
2024-02-04 16:54:31 +0100

    Début Page Game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameCardComponent } from './game-card/game-card.component';
 
 @NgModule({
   declarations: [AppComponent],
