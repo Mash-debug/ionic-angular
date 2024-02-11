95ef54f44153085dab46adf285158a92c7b485f9
FOFANA <fofana36u@etu.univ-lorraine.fr>
2024-02-11 21:42:30 +0100

    Build et APK fonctionnels

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.angular',
+  appId: 'com.ionic.angular',
   appName: 'ionic-angular',
   webDir: 'www',
   server: {
